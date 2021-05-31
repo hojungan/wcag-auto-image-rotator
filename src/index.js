341e91cee8eca55c15b45183258715f5563bc06b
Hojung <hojung1111@gmail.com>
2021-05-31 02:32:43 -0400

    re-worked methods. need to figure out how to use setinterval instead of settimeout

--- src/index.js
+++ src/index.js
@@ -1,144 +1,112 @@
 class SIRModule {
-    constructor(carouselDOMNode) {
-      this.carousel = carouselDOMNode;
-      this.nextBtn = carouselDOMNode.querySelector(".wtm-sir-next");
-      this.prevBtn = carouselDOMNode.querySelector(".wtm-sir-prev");
-      this.playpause = carouselDOMNode.querySelector(".wtm-sir-rotation.wtm-sir-pause");
-      this.carouselItems = carouselDOMNode.querySelectorAll(".wtm-sir-carousel-item");
-      this.liveRegion = carouselDOMNode.querySelector(".wtm-sir-carousel-items");
-      this.pauseIconClass = "fa-pause";
-      this.playIconClass = "fa-play";
-      this.pauseClass = "wtm-sir-pause";
-      this.playClass = "wtm-sir-play";
-      this.playLabel = "Pause automatic slide show";
-      this.pauseLabel = "Play automatic slide show";
-      this.active = "wtm-sir-active";
-      this.autoRotateState = null; // Holds reference to SetInterval
-      this.rotateSpeed = 4000; // Image roates every n seconds
-      this.rotateIndex = 0; // Current index
-    }
-  
-    initiateCarousel() {
-      // Display first item on page load
-      this.carouselItems[0].classList.add(this.active);
-  
-      let btn = this.carousel.querySelector(
-        ".wtm-sir-carousel-inner .wtm-sir-controls .wtm-sir-rotation"
-      );
-      if (btn) {
-        btn.addEventListener("click", (e) => {
-          e.preventDefault();
-          this.handlePlayPauseButtonClick(this.playpause);
-        });
-      }
-  
-      // Add focus and hover event listener to each carousel item
-      for (let i = 0; i < this.carouselItems.length; i++) {
-        let anchor = this.carouselItems[i].querySelector("a");
-  
-        // Focus from Tab key
-        anchor.addEventListener("focus", (e) => {
-          e.preventDefault();
-          if(this.liveRegion.getAttribute('aria-live') === 'off' && this.playpause.classList[1].includes('pause')) {
-            this.handleLiveRegionAnnouncement('polite')
-            this.pauseCarousel()
-          }
-        });
-  
-        anchor.addEventListener("blur", (e) => {
-          e.preventDefault();
-          if(this.liveRegion.getAttribute('aria-live') === 'polite' && this.playpause.classList[1].includes('play')) {
-            this.handleLiveRegionAnnouncement('off')
-            this.playCarousel()
-          }
-        });
-  
-        anchor.addEventListener("mouseover", (e) => {
-          e.preventDefault();
-          if(this.liveRegion.getAttribute('aria-live') === 'off') {
-            this.handleLiveRegionAnnouncement('polite')
-            
-          }
-  
-        });
-  
-        anchor.addEventListener("mouseout", (e) => {
-          e.preventDefault();
-          this.handleLiveRegionAnnouncement('off')
-        });
-      }
-  
-      this.playCarousel();
-    }
-  
-    togglePlayPause(removeClass, addClass, label) {
-      this.playpause.childNodes[1].classList.remove(removeClass);
-      this.playpause.childNodes[1].classList.add(addClass);
-      this.playpause.setAttribute("aria-label", label);
-    }
-  
-    goNext() {
-      this.carouselItems[this.rotateIndex].classList.remove(this.active);
-      this.rotateIndex = (this.rotateIndex + 1) % this.carouselItems.length;
-      this.carouselItems[this.rotateIndex].classList.add(this.active);
-    }
-  
-    goPrev() {
-      this.carouselItems[this.rotateIndex].classList.remove(this.active);
-      this.rotateIndex =
-        (this.rotateIndex - 1 + this.carouselItems.length) %
-        this.carouselItems.length;
-      this.carouselItems[this.rotateIndex].classList.add(this.active);
-    }
-  
-    playCarousel() {
-      this.handleLiveRegionAnnouncement('off')
-  
-      this.autoRotateState = setInterval(() => {
-        this.goNext();
-      }, this.rotateSpeed);
-    }
-  
-    pauseCarousel() {
-      this.handleLiveRegionAnnouncement('polite')
-      clearInterval(this.autoRotateState);
-    }
-  
-  
-    handleFocus(focusType, elem) {
-      console.log(elem)
-    }
-  
-    handlePlayPauseButtonClick(playBtn) {
-      switch (playBtn.classList[1]) {
-        case this.pauseClass:
-          // Carousel is playing now. Pause it
-          this.togglePlayPause(
-            this.pauseIconClass,
-            this.playIconClass,
-            this.pauseLabel
-          );
-          this.pauseCarousel();
-          playBtn.classList.remove(this.pauseClass)
-          playBtn.classList.add(this.playClass)
-          break;
-  
-        case this.playClass:
-          // Carousel is paused. Play it
-          this.togglePlayPause(
-            this.playIconClass,
-            this.pauseIconClass,
-            this.playLabel
-          );
-          this.playCarousel();
-          playBtn.classList.remove(this.playClass)
-          playBtn.classList.add(this.pauseClass)
-          break;
+  constructor(domNode) {
+    this.isPlaying = true
+    this.hasFocus = false
+    this.hasHover = false
+    this.speed = 2500
+    this.currentIndex = 0
+    this.rotationState = null
+
+    this.playLabel = 'Pause automatic slide show'
+    this.pauseLabel = 'Play automatic slide show'
+    this.playIcon = 'fa-play'
+    this.pauseIcon = 'fa-pause'
+
+    this.domNode = domNode
+    this.pauseBtn = domNode.querySelector('.wtm-sir-rotation')
+    this.liveRegion = domNode.querySelector('.wtm-sir-carousel-items')
+    this.carouselItems = domNode.querySelectorAll('.wtm-sir-carousel-item')
+    this.buttons = domNode.querySelectorAll('.wtm-sir-controls button')
+  }
+
+
+  init() {
+    this.carouselItems[0].classList.add('wtm-sir-active')
+
+    for (let i = 0; i < this.buttons.length; i++) {
+      let btn = this.buttons[i]
+
+      if (btn.classList.contains('wtm-sir-rotation')) {
+        btn.addEventListener('click', (e) => {
+          this.togglePauseIcon()
+          this.isPlaying = !this.isPlaying
+          this.isPlaying ? this.playCarousel() : this.pauseCarousel()
+        })
+
+      } else {
+        if (btn.classList.contains('wtm-sir-next')) {
+          btn.addEventListener('click', this.goNext.bind(this))
+        } else {
+          btn.addEventListener('click', this.goBack.bind(this))
+        }
+
+        btn.addEventListener('focus', this.handleFocus.bind(this))
+        btn.addEventListener('blur', this.handleBlur.bind(this))
+
       }
     }
-  
-    handleLiveRegionAnnouncement(method) {
-      this.liveRegion.setAttribute("aria-live", method);
+
+    for (let j = 0; j < this.carouselItems.length; j++) {
+      let elem = this.carouselItems[j]
+
+      // elem.addEventListener('focus', this.handleFocus.bind(this))
+      // elem.addEventListener('blur', this.handleBlur.bind(this))
+    }
+
+    this.domNode.addEventListener('mouseover', this.handleFocus.bind(this))
+    this.domNode.addEventListener('mouseout', this.handleBlur.bind(this))
+
+    setTimeout(this.playCarousel.bind(this),)
+  }
+
+  goNext() {
+    this.carouselItems[this.currentIndex].classList.remove('wtm-sir-active')
+    this.currentIndex = (this.currentIndex + 1) % this.carouselItems.length
+    this.carouselItems[this.currentIndex].classList.add('wtm-sir-active')
+  }
+
+  goBack() {
+    this.carouselItems[this.currentIndex].classList.remove('wtm-sir-active')
+    this.currentIndex = (this.currentIndex - 1 + this.carouselItems.length) % this.carouselItems.length
+    this.carouselItems[this.currentIndex].classList.add('wtm-sir-active')
+  }
+
+  playCarousel() {
+    if (!this.hasHover && !this.hasFocus && this.isPlaying) {
+      this.liveRegion.setAttribute('aria-live', 'off')
+      this.rotationState = setInterval(this.goNext.bind(this), this.speed)
     }
   }
-  
+
+  pauseCarousel() {
+    this.liveRegion.setAttribute('aria-live', 'polite')
+    clearInterval(this.rotationState)
+  }
+
+  togglePauseIcon() {
+    let svg = this.domNode.querySelector('.wtm-sir-rotation .fa-lg')
+    if (this.isPlaying) {
+      svg.classList.remove(this.pauseIcon)
+      svg.classList.add(this.playIcon)
+      this.pauseBtn.setAttribute('aria-label', this.pauseLabel)
+    }
+    else {
+      svg.classList.remove(this.playIcon)
+      svg.classList.add(this.pauseIcon)
+      this.pauseBtn.setAttribute('aria-label', this.playLabel)
+    }
+  }
+
+  // Event Handlers
+  handleFocus(e) {
+    if (!this.pauseBtn.contains(e.target) && this.rotationState == null) {
+      this.hasHover = true
+      this.pauseCarousel()
+    }
+  }
+
+  handleBlur(e) {
+    this.hasHover = false
+    this.playCarousel()
+  }
+}
